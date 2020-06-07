3f4808f331ba846e80448a57f64549e12a1cbbcb
dtorre17 <dtorre17@jhu.edu>
2020-06-07 18:56:45 -0500

    first commit

---
+++ yes/app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", event => {
+    
+    const app = firebase.app();
+    
+
+});
+
+function uploadFile(files) {
+    const storageRef = firebase.storage().ref();
+    const horseRef = storageRef.child('horse.jpg');
+
+    const file = files.item(0);
+
+    const task = horseRef.put(file);
+
+    task.then(snapshot => {
+        console.log(snapshot)
+        const url = snapshot.downloadURL
+        document.querySelector('#imgUpload').setAttribute('src',url)
+    })
+        
+}
