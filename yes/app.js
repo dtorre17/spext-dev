1b6b3b9bcd7388effd47987e3b933baa65ecb2d3
dtorre17 <dtorre17@jhu.edu>
2020-06-08 14:28:59 -0500

    frontend done

--- yes/app.js
+++ yes/app.js
@@ -1,10 +1,67 @@
-document.addEventListener("DOMContentLoaded", event => {
+'use strict';
+
+//grab a form
+const form = document.querySelector('.form-inline');
+
+//grab an input
+const podcastName = form.querySelector('#podcastName');
+
+const podcastURL = form.querySelector('#podcastURL');
+
+
+/*document.addEventListener("DOMContentLoaded", event => {
     
     const app = firebase.app();
     
 
-});
+});*/
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCjU3LdLirPE5vk5CfbNVxx-Jch_8rpoQI",
+    authDomain: "dt-practice.firebaseapp.com",
+    databaseURL: "https://dt-practice.firebaseio.com",
+    projectId: "dt-practice",
+    storageBucket: "dt-practice.appspot.com",
+    messagingSenderId: "931808548280",
+    appId: "1:931808548280:web:e9ba50ee9fb864311aab4d",
+    measurementId: "G-38YHSQH40N"
+  };
+
+//create a functions to push
+function firebasePush(input) {
+
+    //prevents from breaking
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+
+    //push itself
+    var podcastNameRef = firebase.database().ref('podcastNames').push().set(
+        {
+            pcName: input.value
+        }
+    );
+
+    var podcastURLRef = firebase.database().ref('podcastURLs').push().set(
+        {
+            pcURL: input.value
+        }
+    );
+
+}
+
+//push on form submit
+if (form) {
+    form.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+        firebasePush(inputPCName);
+        firebasePush(inputPCURL);
+        //shows alert if everything went well.
+        return alert('Data Successfully Sent to Realtime Database');
+    })
+}
+
+/*
 function uploadFile(files) {
     const storageRef = firebase.storage().ref();
     const horseRef = storageRef.child('horse.jpg');
@@ -19,4 +76,4 @@ function uploadFile(files) {
         document.querySelector('#imgUpload').setAttribute('src',url)
     })
         
-}
+}*/
