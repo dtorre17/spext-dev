6f050ee76e96017179231d3cde8d574c74f0a11a
dtorre17 <dtorre17@jhu.edu>
2020-06-08 17:07:26 -0500

    going to try YT tutorial now

--- yes/app.js
+++ yes/app.js
@@ -1,14 +1,4 @@
-//grab a form
-
-var formData = {
-    "inputPCName": $('inputPCName').val(),
-    "inputPCURL": $('inputPCURL').val(),
-    "inputDescription": $('inputDescription').val(),
-    "inputUserName": $('inputUserName').val(),
-    "inputEmail": $('inputEmail').val(),
-}
-
-const firebaseConfig = {
+const config = {
     apiKey: "AIzaSyCjU3LdLirPE5vk5CfbNVxx-Jch_8rpoQI",
     authDomain: "dt-practice.firebaseapp.com",
     databaseURL: "https://dt-practice.firebaseio.com",
@@ -17,18 +7,36 @@ const firebaseConfig = {
     messagingSenderId: "931808548280",
     appId: "1:931808548280:web:e9ba50ee9fb864311aab4d",
     measurementId: "G-38YHSQH40N"
-  };
+};
 
-//push on form submit
-if (form) {
+firebase.initializeApp(config);
+
+var requests = firebase.database().ref("requests");
+
+var submitRequest = function() {
     
-    $('#myForm').submit(function(evt) {
-        evt.preventDefault(); //Prevent the default form submit action
-      
-        // You have formData here and can do this:
-        firebase.initializeApp(firebaseConfig); //Initialize your firebase here passing your firebase account config object
-        firebase.database().ref('/formDataTree').push( formData ); // Adds the new form data to the list under formDataTree node
-        return alert('Podcast Submitted!');
-      })
+    var inputPCName = $("#inputPCName").val();
+    var inputPCURL = $("#inputPCURL").val();
+    var inputDescription = $("#inputDescription").val();
+    var inputUserName = $("#inputUserName").val();
+    var inputEmail = $("#inputEmail").val();
 
-}
+    
+
+    requests.push({
+        "inputPCName": inputPCName,
+        "inputPCURL" : inputPCURL,
+        "inputDescription" : inputDescription,
+        "inputUserName": inputUserName,
+        "inputEmail" : inputEmail
+    });
+
+};
+
+$(window).load(function () {
+
+    // Find the HTML element with the id recommendationForm, and when the submit
+    // event is triggered on that element, call submitRecommendation.
+    $("#myForm").submit(submitRequest);
+  
+});
