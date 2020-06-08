76e7ee3fc8d15c3c98edfbd0f6e0b46883b45d0c
dtorre17 <dtorre17@jhu.edu>
2020-06-08 16:22:27 -0500

    with comments from codeopen tutorial

--- yes/app.js
+++ yes/app.js
@@ -1,15 +1,30 @@
 'use strict';
 
 //grab a form
+
+var formData = {
+    "inputPCName": $('inputPCName').val(),
+    "inputPCURL": $('inputPCURL').val(),
+    "inputDescription": $('inputDescription').val(),
+    "inputUserName": $('inputUserName').val(),
+    "inputEmail": $('inputEmail').val(),
+}
+
+
+/*
 const form = document.querySelector('.form-inline');
+//grab podcast name
+const inputPCName = form.querySelector('#inputPCName');
+//grab podcast URL
+const inputPCURL = form.querySelector('#inputPCURL');
+//grab podcast add info
+const inputDescription = form.querySelector('#inputDescription');
+//grab user name
+const inputUserName = form.querySelector('#inputUserName');
+//grab user email
+const inputEmail = form.querySelector('#inputEmail');
 
-//grab an input
-const podcastName = form.querySelector('#podcastName');
-
-const podcastURL = form.querySelector('#podcastURL');
-
-
-/*document.addEventListener("DOMContentLoaded", event => {
+document.addEventListener("DOMContentLoaded", event => {
     
     const app = firebase.app();
     
@@ -27,9 +42,9 @@ const firebaseConfig = {
     measurementId: "G-38YHSQH40N"
   };
 
+/*
 //create a functions to push
 function firebasePush(input) {
-
     //prevents from breaking
     if (!firebase.apps.length) {
         firebase.initializeApp(config);
@@ -41,24 +56,50 @@ function firebasePush(input) {
             pcName: input.value
         }
     );
-
     var podcastURLRef = firebase.database().ref('podcastURLs').push().set(
         {
             pcURL: input.value
         }
     );
-
-}
+    var podcastAddInfoRef = firebase.database().ref('podcastInfos').push().set(
+        {
+            pcAddInfo: input.value
+        }
+    );
+    var userNameRef = firebase.database().ref('userNames').push().set(
+        {
+            usrName: input.value
+        }
+    );
+    var userEmail = firebase.database().ref('userEmails').push().set(
+        {
+            usrEmail: input.value
+        }
+    );
+}*/
 
 //push on form submit
 if (form) {
+    
+    $('#myForm').submit(function(evt) {
+        evt.preventDefault(); //Prevent the default form submit action
+      
+        // You have formData here and can do this:
+        firebase.initializeApp(firebaseConfig); //Initialize your firebase here passing your firebase account config object
+        firebase.database().ref('/formDataTree').push( formData ); // Adds the new form data to the list under formDataTree node
+        return alert('Podcast Submitted!');
+      })
+    /*
     form.addEventListener('submit', function (evt) {
         evt.preventDefault();
         firebasePush(inputPCName);
         firebasePush(inputPCURL);
+        firebasePush(inputDescription);
+        firebasePush(inputUserName);
+        firebasePush(inputEmail);
         //shows alert if everything went well.
-        return alert('Data Successfully Sent to Realtime Database');
-    })
+        return alert('Podcast Submitted!');
+    })*/
 }
 
 /*
