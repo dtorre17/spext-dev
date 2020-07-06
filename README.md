f29f473255c519cc7388ef63ca98f480ed7c3efe
dtorre17 <dtorre17@jhu.edu>
2020-07-05 23:23:54 -0500

    Realtime database upload not working

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 `firebase serve`
 
 `visit localhost:5000/`
+
+or
+
+
+`firebase deploy`
+
+`visit https://dt-practice.web.app`
--- yes/upload.js
+++ yes/upload.js
@@ -1,7 +1,7 @@
 const config = {
   apiKey: "AIzaSyCjU3LdLirPE5vk5CfbNVxx-Jch_8rpoQI",
   authDomain: "dt-practice.firebaseapp.com",
-  databaseURL: "https://dt-podcastuploads.firebaseio.com/",
+  databaseURL: "https://dt-practice.firebaseio.com",
   projectId: "dt-practice",
   storageBucket: "dt-practice.appspot.com",
   messagingSenderId: "931808548280",
@@ -10,31 +10,17 @@ const config = {
 };
 
 firebase.initializeApp(config);
-var requests = firebase.database().ref("/published-episodes/");
-var selectedFile;
 
 function getID(id) {
   return document.getElementById(id).value;
 }
 
-/*
-$("#file").on("change", function(event) {
-  
-});*/
-  
+var selectedFile;
+var downloadURL;
+
 document.getElementById("file").addEventListener("change",(e) => {
       e.preventDefault();
       
-      
-      // uploadForm.set({
-      //     podcastName : getID("inputPCName"),
-      //     episodeName : getID("inputEpisodeName"),
-      //     podcastInfo : getID("inputDescription"),
-      //     userEmail : getID("inputEmail"),
-      //     rSS : getID("inputRSS"),
-      //     fileUpload : getID("file")
-      // });
-
       selectedFile = event.target.files[0];
       var filename = selectedFile.name;
       var storageRef = firebase.storage().ref().child(filename);
@@ -45,71 +31,25 @@ document.getElementById("file").addEventListener("change",(e) => {
       }, function(error) {
           //unsuccesful
       }, function() {
-          var downloadURL = uploadTask.snapshot.downloadURL;
+          downloadURL = uploadTask.snapshot.downloadURL;
           console.log(downloadURL);
       });
-
-
-      // var uploadForm = uploads.push();
-      
-      // alert("Request submitted!");
-      // console.log("Request submitted");
-      // document.getElementById("uploads").reset();
-      // window.location.href = 'https://publish.spext.co';
 });
 
+var uploads = firebase.database().ref("/published-episodes");
 
-/* code from Google git documentation
-    var auth = firebase.auth();
-    var storageRef = firebase.storage().ref();
+document.getElementById("form").addEventListener("submit",(e) => {
+  e.preventDefault();
+  var uploadForm = uploads.push();
+  
+  uploadForm.set({
+       podcastName : getID("inputPCName"),
+       episodeName : getID("inputEpisodeName"),
+       podcastInfo : getID("inputDescription"),
+       userEmail : getID("inputEmail"),
+       rSS : getID("inputRSS"),
+       fileUpload : getID("file")
+   });
+   alert("Request submitted!");
 
-    function handleFileSelect(evt) {
-      evt.stopPropagation();
-      evt.preventDefault();
-      var file = evt.target.files[0];
-
-      var metadata = {
-        'contentType': file.type
-      };
-
-      // Push to child path.
-      // [START oncomplete]
-      storageRef.child('images/' + file.name).put(file, metadata).then(function(snapshot) {
-        console.log('Uploaded', snapshot.totalBytes, 'bytes.');
-        console.log('File metadata:', snapshot.metadata);
-        // Let's get a download URL for the file.
-        snapshot.ref.getDownloadURL().then(function(url) {
-          console.log('File available at', url);
-          // [START_EXCLUDE]
-          document.getElementById('linkbox').innerHTML = '<a href="' +  url + '">Click For File</a>';
-          // [END_EXCLUDE]
-        });
-      }).catch(function(error) {
-        // [START onfailure]
-        console.error('Upload failed:', error);
-        // [END onfailure]
-      });
-      // [END oncomplete]
-    }
-
-    window.onload = function() {
-      document.getElementById('file').addEventListener('change', handleFileSelect, false);
-      document.getElementById('file').disabled = true;
-
-      auth.onAuthStateChanged(function(user) {
-        if (user) {
-          console.log('Anonymous user signed-in.', user);
-          document.getElementById('file').disabled = false;
-        } else {
-          console.log('There was no anonymous session. Creating a new anonymous user.');
-          // Sign the user in anonymously since accessing Storage requires the user to be authorized.
-          auth.signInAnonymously().catch(function(error) {
-            if (error.code === 'auth/operation-not-allowed') {
-              window.alert('Anonymous Sign-in failed. Please make sure that you have enabled anonymous ' +
-                  'sign-in on your Firebase project.');
-            }
-          });
-        }
-      });
-    }
-*/
+});
